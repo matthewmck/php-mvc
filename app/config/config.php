feaf55ab5cc4df4076d5eaac26988946163e7d73
Matt <matthew.mckee@live.com>
2019-07-22 17:48:32 -0400

    init commit

---
+++ app/config/config.php
@@ -0,0 +1,13 @@
+<?php
+  // DB Params
+  define('DB_HOST', 'localhost');
+  define('DB_USER', '_YOUR_USER_ACCOUNT_');
+  define('DB_PASS', '_YOUR_PASSWORD_');
+  define('DB_NAME', '_YOUR_DB_NAME_');
+
+  // App Root
+  define('APPROOT', dirname(dirname(__FILE__)));
+  // URL Root
+  define('URLROOT', '_YOUR_URL_');
+  // Site Name
+  define('SITENAME', '_YOUR_SITENAME_');
